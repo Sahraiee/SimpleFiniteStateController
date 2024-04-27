f2a2f010b69e94550ef7aabc90a8e86031f87d40
Hamid Sahraiee <sahraiee@gmail.com>
2024-04-28 00:59:34 +0330

    Update namespace

---
+++ SahraCore/FSM/IFsmController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace SahraCore.FSM
+{
+    public interface IFsmController<E> where E : Enum
+    {
+        FsmNode<E> CurrentState { get; }
+        public E CurrentStateID { get; }
+
+        void AddState(E id, Action onStateEnter = null, Action onStateExit = null, Action onStateStay = null);
+        void AddState(FsmNode<E> _node);
+        void AddOverrideState(FsmNode<E> _node);
+        void RemoveState(FsmNode<E> _node);
+        void ChangeState(E _stateID, bool allowToRepeate = false);
+        void UpdateTask();
+        void SetEnableStayTask(bool enableUpdateTask);
+    }
+
+}
